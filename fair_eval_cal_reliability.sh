4447a4d9b77538080c5d08284468f65fc78298bd
semwett0301 <semen.mokrov@gmail.com>
2025-05-16 16:06:44 +0200

    feat: setted up the models

--- fair_eval_cal_reliability.sh
+++ fair_eval_cal_reliability.sh
@@ -1,8 +1,8 @@
 export CUDA_VISIBLE_DEVICES=0
 
-MODEL_PATH=./models/_
-BASE_MODEL_PATH=./models/_
-MODEL_TYPE=judgelm
+MODEL_PATH=./models/autoj-13b
+BASE_MODEL_PATH=./models/Mistral-7B
+MODEL_TYPE=auto-j
 DATA_TYPE=vicuna
 
 
