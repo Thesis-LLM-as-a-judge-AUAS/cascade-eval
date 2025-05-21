cb05ee6cc3e7eed8c3ee0fd830d7b3bb60c80a0a
semwett0301 <semen.mokrov@gmail.com>
2025-05-21 02:00:08 +0200

    feat: new cascaded eval setup

--- fair_eval_with_cascade_eval.sh
+++ fair_eval_with_cascade_eval.sh
@@ -11,3 +11,14 @@ for i in $(seq 1 47); do
       --logit-file-gpt "outputs/expanded_review/${i}_review_gpt35_vicuna_gpt-3.5-turbo_mec3_bpc1.jsonl" \
       --final-output-file "outputs/final-outputs/${DATA_TYPE}/${i}-${MODEL_TYPE}-${DATA_TYPE}-final.json"
 done
+
+MODEL_TYPE="auto-j"
+
+for i in $(seq 1 47); do
+  python3 -u src/cascaded_eval.py \
+      --data-type $DATA_TYPE \
+      --logit-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-logit.jsonl" \
+      --output-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-relia.json" \
+      --logit-file-gpt "outputs/expanded_review/${i}_review_gpt35_vicuna_gpt-3.5-turbo_mec3_bpc1.jsonl" \
+      --final-output-file "outputs/final-outputs/${DATA_TYPE}/${i}-${MODEL_TYPE}-${DATA_TYPE}-final.json"
+done
