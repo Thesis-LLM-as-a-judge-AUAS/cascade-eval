707e80381d3f02cdefa8d00c919e36a00fa65ecc
semwett0301 <semen.mokrov@gmail.com>
2025-05-23 13:57:12 +0200

    feat: mec gpt-4 setup

--- fair_eval_with_cascade_eval.sh
+++ fair_eval_with_cascade_eval.sh
@@ -1,14 +1,14 @@
 export CUDA_VISIBLE_DEVICES=0
 
 MODEL_TYPE="judgelm"
-DATA_TYPE="vicuna-gpt4"
+DATA_TYPE="vicuna-mec-gpt4"
 
 for i in $(seq 1 47); do
   python3 -u src/cascaded_eval.py \
       --data-type $DATA_TYPE \
       --logit-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-logit.jsonl" \
       --output-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-relia.json" \
-      --logit-file-gpt "outputs/fair-eval/${i}/review_gpt35_vicuna_gpt-4_mec1_bpc0.jsonl" \
+      --logit-file-gpt "outputs/expanded_review/review_gpt35_vicuna_gpt-4_mec3_bpc1/${i}_review_gpt35_vicuna_gpt-4_mec3_bpc1.jsonl" \
       --final-output-file "outputs/final-outputs/${DATA_TYPE}/${i}-${MODEL_TYPE}-${DATA_TYPE}-final.json"
 done
 
@@ -19,6 +19,6 @@ for i in $(seq 1 47); do
       --data-type $DATA_TYPE \
       --logit-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-logit.jsonl" \
       --output-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-relia.json" \
-      --logit-file-gpt "outputs/fair-eval/${i}/review_gpt35_vicuna_gpt-4_mec1_bpc0.jsonl" \
+      --logit-file-gpt "outputs/expanded_review/review_gpt35_vicuna_gpt-4_mec3_bpc1/${i}_review_gpt35_vicuna_gpt-4_mec3_bpc1.jsonl" \
       --final-output-file "outputs/final-outputs/${DATA_TYPE}/${i}-${MODEL_TYPE}-${DATA_TYPE}-final.json"
 done
