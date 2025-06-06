81a1401f5b313777806ba584ddbc9c4f2e31268f
semwett0301 <semen.mokrov@gmail.com>
2025-06-06 12:30:28 +0200

    feat: cascaded eval changes

--- fair_eval_with_cascade_eval.sh
+++ fair_eval_with_cascade_eval.sh
@@ -12,11 +12,11 @@ for i in $(seq 1 47); do
       --final-output-file "outputs/final-outputs/${DATA_TYPE}/${i}-${MODEL_TYPE}-${DATA_TYPE}-final.json"
 done
 
-for i in $(seq 1 47); do
-  python3 -u src/cascaded_eval.py \
-      --data-type $DATA_TYPE \
-      --logit-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-logit.jsonl" \
-      --output-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-relia.json" \
-      --logit-file-gpt "outputs/expanded_review/review_gpt35_vicuna_gpt-4_mec3_bpc1/${i}_review_gpt35_vicuna_gpt-4_mec3_bpc1.jsonl" \
-      --final-output-file "outputs/final-outputs/${DATA_TYPE}/${i}-${MODEL_TYPE}-${DATA_TYPE}-final.json"
-done
+#for i in $(seq 1 47); do
+#  python3 -u src/cascaded_eval.py \
+#      --data-type $DATA_TYPE \
+#      --logit-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-logit.jsonl" \
+#      --output-file1 "relia_scores/${MODEL_TYPE}/${DATA_TYPE}/${i}-relia.json" \
+#      --logit-file-gpt "outputs/expanded_review/review_gpt35_vicuna_gpt-4_mec3_bpc1/${i}_review_gpt35_vicuna_gpt-4_mec3_bpc1.jsonl" \
+#      --final-output-file "outputs/final-outputs/${DATA_TYPE}/${i}-${MODEL_TYPE}-${DATA_TYPE}-final.json"
+#done
