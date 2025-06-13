9e6a250ee4c7e1fd85980ea74bcf0fd1667f032a
semwett0301 <semen.mokrov@gmail.com>
2025-06-13 12:08:48 +0200

    feat: prepare for Vicuna

--- src/build_prompt_judge.py
+++ src/build_prompt_judge.py
@@ -150,17 +150,30 @@ Write critiques for this response. After that, you should give a final rating fo
 Write critiques for this response. After that, you should give a final rating for the response on a scale of 1 to 10 by strictly following this format: "[[rating]]", for example: "Rating: [[5]]". [/INST]"""
 
         else:
-            instruction = """We would like to request your feedback on the performance of two AI assistants in response to the user question displayed above.
-    Please rate the helpfulness, relevance, accuracy, level of details of their responses. 
-
-    Each assistant receives an overall score on a scale of 1 to 10, where a higher score indicates better overall performance.
-    Please first provide a comprehensive explanation of your evaluation, avoiding any potential bias and ensuring that the order in which the responses were presented does not affect your judgment. 
-    Then, output two lines indicating the scores for Assistant 1 and 2, respectively.
-
-    Output with the following format:
-    Evaluation evidence: <your evaluation explanation here>
-    Score of the Assistant 1: <score>
-    Score of the Assistant 2: <score>"""
+            instruction = """[Question]
+            {question_body}
+            
+            [The Start of Assistant 1's Answer]
+            {answer1_body}
+            [The End of Assistant 1's Answer]
+            
+            [The Start of Assistant 2's Answer]
+            {answer2_body}
+            [The End of Assistant 2's Answer]
+            
+            [System]
+            We would like to request your feedback on the performance of two AI assistants in response to the user question displayed above.
+            Please rate the helpfulness, relevance, accuracy, level of details of their responses. 
+        
+            Each assistant receives an overall score on a scale of 1 to 10, where a higher score indicates better overall performance.
+            Please first provide a comprehensive explanation of your evaluation, avoiding any potential bias and ensuring that the order in which the responses were presented does not affect your judgment. 
+            Then, output two lines indicating the scores for Assistant 1 and 2, respectively.
+        
+            Output with the following format:
+            Evaluation evidence: <your evaluation explanation here>
+            Score of the Assistant 1: <score>
+            Score of the Assistant 2: <score>
+            """
 
     elif model_type == "prometheus":
         instruction = """[INST] <<SYS>>
@@ -279,8 +292,9 @@ def parse_predictions(predictions, model_type, data_type, prompt_type):
                 return 3  # default is middle score
 
     def parse_score_autoj(review, is_pair=True):
-        matches = re.findall(r'Assistant\s+\d:\s*(\d+)', review)
-        return [int(score) for score in matches]
+        pattern = r"Assistant\s*1:\s*([0-9]+(?:\.[0-9]+)?)\s*.*?Assistant\s*2:\s*([0-9]+(?:\.[0-9]+)?)"
+        match = re.search(pattern, review, re.DOTALL | re.IGNORECASE)
+        return [float(match.group(1)), float(match.group(2))]
 
         # if is_pair:
 
